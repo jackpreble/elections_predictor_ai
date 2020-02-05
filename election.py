815c99eef33d42dd134d72772e93ecb62c3bf73b
jackpreble <60149852+jackpreble@users.noreply.github.com>
2020-02-05 11:51:28 -0500

    2/5/20, 11:51a

--- election.py
+++ election.py
@@ -1,10 +1,11 @@
 class Election:
 
-    def __init__(self, year, state, special, totalVotes):
+    def __init__(self, year, state, party, totalVotes, winner):
         self.year = year
         self.state = state
-        self.candidates = []
+        self.party = party
         self.totalVotes = totalVotes
+        self.winner = winner
 
     def add_candidate(self, candidate):
         self.candidates.append(candidate)
@@ -18,3 +19,21 @@ class Election:
     #def election_winner(self):
         #for election in election_results:
             #if election[1] == election[1] and election[0] == election[0]:
+    def __eq__(self, other):
+        if isinstance(other, Election):
+            return self.year == other.year and self.state == other.state
+        return False
+
+    def __lt__(self, other):
+        if self.state == other.state:
+            return self.year < other.year
+        else:
+            return self.state < other.state
+
+    '''def __le__(self, other):
+        if self.state == other.state:
+            return self.year <= other.year
+        else:
+            return self.state <= other.state'''
+
+    def __gt__(self, other):
